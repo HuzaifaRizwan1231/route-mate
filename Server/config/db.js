07346d07a340cb8afff872e87d255f70d14e8a26
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-07-04 13:15:44 +0500

    connection with db established

---
+++ Server/config/db.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql')
+
+// const db = mysql.createPool({
+//     waitForConnections : true,
+//     queueLimit :0,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     debug    :  true,
+//     wait_timeout : 28800,
+//     connect_timeout :10
+// })
+
+const db = mysql.createPool({
+    waitForConnections : true,
+    queueLimit :0,
+    host: "mysql-108e726a-huzaifa-a95e.d.aivencloud.com",
+    user: "avnadmin",
+    password: "AVNS_jGXuSgbPuvpR5YAL9TQ",
+    database: "fast-travel",
+    debug    :  true,
+    wait_timeout : 28800,
+    connect_timeout :10,
+    port:11134
+})
+
+
+module.exports = db;
--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,20 @@ const cors = require("cors")
 const app = express()
 const PORT = 3000;
 
+const db  = require("./config/db.js")
+
 app.use(cors())
 app.use(express.json())
 
+// Queries
+app.get("/users", (req, res)=>{
+    db.query("SELECT * FROM User",(err, result)=>{
+        if (err){
+            return res.json(err);
+        }
+        return res.json(result)
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`)
